f930690ee0abf6a0a3b1319adf2266f761f22564
mustachemo <mhasan1080@gmail.com>
2023-08-27 11:02:49 -0700

    updated models

--- src/models/cities.js
+++ src/models/cities.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
 const imgFileSchema = new mongoose.Schema({
-  filename: String,
-  originalName: String,
-  mimeType: String,
-  size: Number,
-  data: Buffer,
+  name: String,
+  desc: String,
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
 });
 
 const citySchema = new mongoose.Schema(
