653599f1e074b8c145337c6a81dd6b870a7ab114
mustachemo <mhasan1080@gmail.com>
2023-08-27 09:35:28 -0700

    styles showing!

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(cors());
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'));
 
 app.use('/', indexRouter);
 // app.use('/new', newMessageRouter);
