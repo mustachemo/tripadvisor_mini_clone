5229f965a64151e6f4edcd0a6528373397e6710a
mustachemo <mhasan1080@gmail.com>
2023-08-28 23:23:14 -0700

    added model functions!
    Great info!

--- src/models/cities.js
+++ src/models/cities.js
@@ -36,5 +36,41 @@ const citySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+citySchema.pre('save', async function (next) {
+  const city = this;
+
+  if (city.isModified('image')) {
+    city.image = await sharp(city.image).resize(250, 250).png().toBuffer();
+  }
+
+  next();
+});
+
+citySchema.pre('remove', async function (next) {
+  const city = this;
+  await Attraction.deleteMany({ city: city._id });
+  next();
+});
+
+// this is a method that is available on the instance of the model
+// it is called when the model is converted to JSON, which happens when we send it back to the client
+// it is automatically called by express when we send the response
+citySchema.methods.toJSON = function () {
+  const city = this;
+  const cityObject = city.toObject();
+
+  delete cityObject.image;
+  delete cityObject.attractions;
+
+  return cityObject;
+};
+
+// this is a method that is available on the model itself
+// it is called when we use res.send() to send the model back to the client
+// it is automatically called by express when we send the response
+citySchema.virtual('url').get(function () {
+  return `/cities/${this._id}`;
+});
+
 export const City = mongoose.model('City', citySchema);
 export const CityImage = mongoose.model('CityImages', imgFileSchema);
