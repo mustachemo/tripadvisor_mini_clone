0f891ae0feb45bd68aed5f38872d41e9f1a93508
mustachemo <mhasan1080@gmail.com>
2023-08-29 12:19:00 -0700

    need to update controllers

--- src/controllers/attractionsController.js
+++ src/controllers/attractionsController.js
@@ -1,8 +1,44 @@
 import { City, CityImage } from '../models/cities';
 import { connectDB } from '../configs/db.config.js';
 
-export const getCities = async (req, res) => {
-    try {
-        await connectDB();
-        const attractions = await 
+export const getAttractions = async (req, res) => {
+  try {
+    await connectDB();
+    const attractions = await City.find().populate('image');
+  } catch {}
+};
+
+export const postAttractions = async (req, res) => {
+  try {
+    await connectDB();
+    const { cityName, cityDesc, cityPop, cityArea, cityAHI } = req.body;
+    let imageToSave = null;
+
+    if (req.file) {
+      const { originalname, buffer, mimetype } = req.file;
+
+      const Image = new CityImage({
+        name: originalname,
+        img: {
+          data: buffer,
+          contentType: mimetype,
+        },
+      });
+
+      await Image.save();
+      imageToSave = Image;
     }
+
+    const newCity = new City({
+      name: cityName,
+      description: cityDesc,
+      image: imageToSave,
+      population: cityPop,
+      area: cityArea,
+      AverageHouseholdIncome: cityAHI,
+    });
+
+    await newCity.save();
+    res.redirect('/');
+  } catch {}
+};
--- src/routes/attractions.js
+++ src/routes/attractions.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import uploadImage from '../middleware/uploadImage.js';
+import { getAttractions, postAttractions } from '../controllers/attractionsController.js';
+import { get } from 'lodash';
 
 const attractionsController = express.Router();
 
-attractionsController.route('/:id').get();
+attractionsController.route('/:id').get(getAttractions).post(uploadImage.single('attractionsImg'), postAttractions);
 
 export default attractionsController;
