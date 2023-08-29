a25fda36e623a87c161e4e54849b6e1f99c555a2
mustachemo <mhasan1080@gmail.com>
2023-08-29 14:06:01 -0700

    resizing now works!

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -38,26 +38,32 @@ export const postCity = async (req, res, next) => {
     if (req.file) {
       const { originalname, buffer, mimetype } = req.file;
 
-      sharp(buffer)
-        .resize(250, 250)
-        .png()
-        .toBuffer((err, resizedBuffer, info) => {
-          if (err) {
-            throw new Error(err);
-          }
-          console.log(info);
-
-          const Image = new CityImage({
-            name: originalname,
-            img: {
-              data: resizedBuffer,
-              contentType: mimetype,
-            },
+      const resizedBuffer = await new Promise((resolve, reject) => {
+        sharp(buffer)
+          .resize(250, 250)
+          .png()
+          .toBuffer((err, resizedBuffer, info) => {
+            if (err) {
+              reject(err);
+            } else {
+              console.log(info);
+              console.log(`Size of image: ${Buffer.byteLength(buffer, 'utf-8')} bytes`);
+              console.log(`Size of resized image: ${Buffer.byteLength(resizedBuffer, 'utf-8')} bytes`);
+              resolve(resizedBuffer);
+            }
           });
+      });
 
-          Image.save();
-          imageToSave = Image;
-        });
+      const Image = new CityImage({
+        name: originalname,
+        img: {
+          data: resizedBuffer,
+          contentType: mimetype,
+        },
+      });
+
+      await Image.save();
+      imageToSave = Image;
     }
 
     const newCity = new City({
