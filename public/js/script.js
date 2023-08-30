a0366542ad9ab5419d76cb2a975857447ccb668e
mustachemo <mhasan1080@gmail.com>
2023-08-29 20:07:05 -0700

    add button for attractions page still not working

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,41 @@
-// make githib icon bounce on hover
-const githubIcon = document.querySelector('footer span i');
+document.addEventListener('DOMContentLoaded', () => {
+  // make githib icon bounce on hover
+  const githubIcon = document.querySelector('footer span i');
 
-githubIcon.addEventListener('mouseover', () => {
-  githubIcon.classList.add('fa-bounce');
-});
+  githubIcon.addEventListener('mouseover', () => {
+    githubIcon.classList.add('fa-bounce');
+  });
 
-githubIcon.addEventListener('mouseout', () => {
-  githubIcon.classList.remove('fa-bounce');
-});
+  githubIcon.addEventListener('mouseout', () => {
+    githubIcon.classList.remove('fa-bounce');
+  });
 
-// click add-icon to open form/modal for cities
-const addCity = document.querySelector('#addCityButton');
-const addCityForm = document.querySelector('#addCityForm');
+  // click add-icon to open form/modal for cities
+  const addCity = document.querySelector('#addCityButton');
+  const addCityForm = document.querySelector('#addCityForm');
 
-addCity.addEventListener('click', () => {
-  addCityForm.showModal();
+  addCity.addEventListener('click', event => {
+    console.log('add city button clicked');
+    addCityForm.showModal();
+  });
 
-  addCityForm.addEventListener('click', e2 => {
-    if (e2.target === addCityForm) {
+  addCityForm.addEventListener('click', e => {
+    if (e.target === addCityForm) {
       addCityForm.close();
     }
   });
-});
 
-// click add-icon to open form/modal for attractions
-const addAttraction = document.querySelector('#addAttractionButton');
-const addAttractionForm = document.querySelector('#addAttractionForm');
+  // click add-icon to open form/modal for attractions
+  const addAttraction = document.querySelector('#addAttractionButton');
+  const addAttractionForm = document.querySelector('#addAttractionForm');
 
-addAttraction.addEventListener('click', () => {
-  console.log('add attraction button clicked');
-  addAttractionForm.showModal();
-  console.log('add attraction form opened');
+  addAttraction.addEventListener('click', () => {
+    console.log('add attraction button clicked');
+    addAttractionForm.showModal();
+  });
 
-  addAttractionForm.addEventListener('click', e2 => {
-    if (e2.target === addAttractionForm) {
+  addAttractionForm.addEventListener('click', e => {
+    if (e.target === addAttractionForm) {
       addAttractionForm.close();
     }
   });
