0e25b40029adae7464fba55a32c6f0005ba86b95
mustachemo <mhasan1080@gmail.com>
2023-08-30 23:06:24 -0700

    made a task

--- src/controllers/attractionsController.js
+++ src/controllers/attractionsController.js
@@ -5,6 +5,10 @@ export const getAttractions = async (req, res, next) => {
   try {
     // await connectDB();
     // const attractions = await City.find().populate('image');
+    console.log('getAttractions');
+    const result = add(1, 2);
+
+    result;
 
     // res.render('attractions', { attractions: attractions });
     res.render('attractions');
