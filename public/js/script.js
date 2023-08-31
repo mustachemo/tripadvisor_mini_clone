b260febbc4dbb49108abddefa7ba1209a9f38619
mustachemo <mhasan1080@gmail.com>
2023-08-31 13:29:18 -0700

    Functionality successful!

--- public/js/script.js
+++ public/js/script.js
@@ -38,24 +38,26 @@ document.addEventListener('DOMContentLoaded', () => {
       deleteDialog.showModal();
 
       confirmDeleteButton.addEventListener('click', async () => {
-        try {
-          const cityId = deleteButton.getAttribute('data-city-id');
-          const response = await fetch(`/${cityId}`, {
-            method: 'DELETE',
-          });
+        // try {
+        const cityId = deleteButton.getAttribute('data-city-id');
+        const response = await fetch(`/${cityId}`, {
+          method: 'DELETE',
+        });
 
-          if (response.ok) {
-            console.log('City deleted successfully');
-            // Perform any further actions or UI updates
-          } else {
-            console.error('Failed to delete city');
-            // Handle error scenario
-          }
+        deleteDialog.close();
 
-          deleteDialog.close();
-        } catch (error) {
-          console.error('An error occurred:', error);
-        }
+        //   if (response.ok) {
+        //     console.log('City deleted successfully');
+        //     // Perform any further actions or UI updates
+        //   } else {
+        //     console.error('Failed to delete city');
+        //     // Handle error scenario
+        //   }
+
+        //   deleteDialog.close();
+        // } catch (error) {
+        //   console.error('An error occurred:', error);
+        // }
       });
 
       cancelDeleteButton.addEventListener('click', () => {
