65105c829e2be1d0d116cbe666417f118b9fa572
mustachemo <mhasan1080@gmail.com>
2023-09-02 00:36:42 -0700

    need to encrypt passwords

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ import logger from 'morgan';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import path from 'path';
-// import session from 'express-session';
-// import LocalStorage from 'passport-local';
+import session from 'express-session';
+import passport from 'passport';
+import { Strategy as localStrategy } from 'passport-local';
 import indexRouter from './routes/index.js';
 import attractionsRouter from './routes/attractions.js';
 import errorHandler from './middleware/errorHandling.js';
@@ -17,7 +18,6 @@ import loginRouter from './routes/login.js';
 // TODO: Use firebase for user authentication, it's easier, free, secure, fast, and is a node module
 
 dotenv.config();
-// LocalStorage.Strategy;
 
 const app = express();
 const port = process.env.PORT;
@@ -38,6 +38,15 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(cors());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/public', express.static('public'));
 
 app.use('/', indexRouter);
