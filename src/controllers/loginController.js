416ea039267a45c5864cea02b17417f6c198060d
mustachemo <mhasan1080@gmail.com>
2023-09-04 01:30:13 -0700

    login succes achieved!

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -7,10 +7,17 @@ export const getLogin = (req, res) => {
 export const postLogin = async (req, res, next) => {
   try {
     passport.authenticate('local', {
-      successRedirect: '/',
       failureRedirect: '/login',
       failureFlash: true,
-    })(req, res, next);
+    })(req, res, (err) => {
+      if (err) {
+        console.log(err);
+        throw new Error(err);
+      }
+
+      req.flash('success', 'Logged in successfully!');
+      res.redirect('/');
+    });
   } catch (err) {
     console.log(err);
     next(err);
